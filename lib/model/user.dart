95ca1c620635cc30b6631efa30134fdcd4a75f22
johncross00 <johncross.ktm@gmail.com>
2024-06-11 14:57:37 +0000

    Params

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,8 +1,13 @@
 class User{
   final String user_name;
   final String email;
+  final String password;
   final String phone;
-  User(this.user_name, {
+
+  final String profile_image;
+  User(this.user_name,
+      this.password,
+      this.profile_image, {
     required this.email,
     required this.phone,
   });
